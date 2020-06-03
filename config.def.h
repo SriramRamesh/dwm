3894baf49e92364b33524352b63692e91e1f9d44
Sriram Ramesh <grksriram321@gmail.com>
2020-06-03 17:01:58 +0530

    Add EWM htags and alter gaps

--- config.def.h
+++ config.def.h
@@ -2,12 +2,12 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int gappx     = 15;        /* gaps between windows */
+static const unsigned int gappx     = 30;        /* 30 gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const int vertpad            = 0;       /* vertical padding of bar */
-static const int sidepad            = 0;       /* horizontal padding of bar */
+static const int vertpad            = 0;       /* 15vertical padding of bar */
+static const int sidepad            = 0;       /* 25horizontal padding of bar */
 static const char *fonts[]          = {"FontAwesome:pixelsize=28:antialias=true:autohint=true", "Hack:size=10"};
 static const char dmenufont[]       = "Hack:size=10";
 static const char col_gray1[]       = "#222222";
