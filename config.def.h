6011b48640160d96e03148473e96c942f1da3aca
Sriram Ramesh <grksriram321@gmail.com>
2020-06-03 12:29:38 +0530

    Move to mod4

--- config.def.h
+++ config.def.h
@@ -8,8 +8,8 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int vertpad            = 0;       /* vertical padding of bar */
 static const int sidepad            = 0;       /* horizontal padding of bar */
-static const char *fonts[]          = {"FontAwesome:pixelsize=28:antialias=true:autohint=true", "Hack:size=12"};
-static const char dmenufont[]       = "Hack:size=12";
+static const char *fonts[]          = {"FontAwesome:pixelsize=28:antialias=true:autohint=true", "Hack:size=10"};
+static const char dmenufont[]       = "Hack:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -57,7 +57,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
