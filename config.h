190bf2de52923e010984bdb115ab9997f61eb855
Sriram Ramesh <grksriram321@gmail.com>
2020-07-24 07:43:34 +0530

    Icons and hide vaccant tags

--- config.h
+++ config.h
@@ -6,7 +6,7 @@ static const int gappx     = 15;                 /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
+static const char *fonts[]          = {"Hack:pixelsize=28","FontAwesome:pixelsize=28",  "Caskaydia Cove Nerd Font:pixelsize=35" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -26,7 +26,7 @@ static const unsigned int alphas[][3]      = {
 	[SchemeSel]  = { OPAQUE, baralpha, borderalpha },
 };
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "", "", "", "", "", "", "", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -35,7 +35,7 @@ static const Rule rules[] = {
 	 */
 	/* class            instance    title       tags mask             isfloating   monitor */
 	{ "Gimp",            NULL,      NULL,       0,                       1,           -1 },
-	{ "st-256color",     NULL,      NULL,       (1 << 0) + (1<<6),       0,           -1 },
+	{ "st-256color",     NULL,      "parthasarathy",       (1 << 0) + (1<<6),       0,           -1 },
 	{ "firefox",         NULL,      NULL,       (1 << 1) + (1<<6),       0,           -1 },
 	{ "Chromium",        NULL,      NULL,       (1 << 1) + (1<<6),       0,           -1 },
 	{ "Emacs",           NULL,      NULL,       (1 << 2) + (1<<6),       0,           -1 },
